a388f17387b51c375f7c4814f32b6be36842a6d3
koerino <koerino@gmail.com>
2016-02-01 22:50:11 -0500

    update and add new template

--- server.js
+++ server.js
@@ -1,28 +1,16 @@
 'use strict';
 
-//load modules
 var express = require('express');
-var http = require('http');
-var routes = require('routes')
 var favicon = require('serve-favicon');
-var errorHandler = require('errorhandler');
-
-//initialize server
 var app = express();
-var server = http.createServer(app);
 
 //environment configuration
 app.set('port', process.env.PORT || 3000);
-app.use(favicon(__dirname + '/public/window-programming.icns'));
+app.use(favicon(__dirname + '/public/assets/favicon.ico'));
 //set static file directory
-app.use('/public', express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
-//errorhanlder in development mode
-if('development' = app.get('env')) {
-    app.use(errorHandler());
-}
-
-port = app.get('port');
-server.listen(port, function() {
+var port = app.get('port');
+app.listen(port, function() {
     console.log('Server listening on localhost: ' + port);
 });
